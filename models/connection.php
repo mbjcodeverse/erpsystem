d35cbfe98678d348a5e847a8d96d3af03510bf3a
mbjcodeverse <markhjolive@gmail.com>
2026-05-21 01:24:07 +0800

    Hostinger test...

--- models/connection.php
+++ models/connection.php
@@ -2,9 +2,9 @@
 
 class Connection{
 	public function connect(){
-		$link = new PDO("mysql:host=localhost;dbname=erp", "root", "");
+		// $link = new PDO("mysql:host=localhost;dbname=erp", "root", "");
 
-		// $link = new PDO("mysql:host=localhost;dbname=u661302059_luispaint", "u661302059_luispaint", "BcdLuisPa1nt!");
+		$link = new PDO("mysql:host=localhost;dbname=u896983687_erp", "u896983687_erp", "ErP_System1!");
 
 		$link -> exec("set names utf8");
 		return $link;
