0ccecff14a401dd874f4d2065eb07607d7b2bbad
mbjcodeverse <markhjolive@gmail.com>
2026-06-01 00:47:10 +0800

    Migrated to mojpos.com

--- models/connection.php
+++ models/connection.php
@@ -4,7 +4,7 @@ class Connection{
 	public function connect(){
 		// $link = new PDO("mysql:host=localhost;dbname=erp", "root", "");
 
-		$link = new PDO("mysql:host=localhost;dbname=u896983687_erp", "u896983687_erp", "ErP_System1!");
+		$link = new PDO("mysql:host=localhost;dbname=u618435671_erp", "u618435671_erp", "ErP_System1!");
 
 		$link -> exec("set names utf8");
 		return $link;
